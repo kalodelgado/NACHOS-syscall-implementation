214459269c4b72a795367513721ec1a465d2a239
Manikanta Reddy <manikant@cse.iitk.ac.in>
2015-09-26 01:44:59 +0530

    Fixed Execv. yay

--- nachos/code/userprog/exception.cc
+++ nachos/code/userprog/exception.cc
@@ -305,47 +305,50 @@ ExceptionHandler(ExceptionType which)
         printf("Returning control to raw programs\n");
         
     }
-    // else if((which == SyscallException) && (type == syscall_Exec)){
-    //     //I assume there is something called filename :(
-    //     //where do I find
+    else if((which == SyscallException) && (type == syscall_Exec)){
+        //I assume there is something called filename :(
+        //where do I find
 
-    //     //refer to line 130 above to know how to read from memory
-    //     char filename[100];
-    //     int i=0;
+        //refer to line 130 above to know how to read from memory
+        char filename[100];
+        int i=0;
 
-    //     vaddr = machine->ReadRegister(4);
-    //     machine->ReadMem(vaddr, 1, &memval);
-    //     while ((*(char*)&memval) != '\0') {
-    //         filename[i]  = (char)memval;
-    //         ++i;
-    //         vaddr++;
-    //         machine->ReadMem(vaddr, 1, &memval);
-    //     }
-    //     filename[i]  = (char)memval;
-    //     //This by far is weirdestthing that has been copied
+        vaddr = machine->ReadRegister(4);
+        machine->ReadMem(vaddr, 1, &memval);
+        while ((*(char*)&memval) != '\0') {
+            filename[i]  = (char)memval;
+            ++i;
+            vaddr++;
+            machine->ReadMem(vaddr, 1, &memval);
+        }
+        filename[i]  = (char)memval;
+        //This by far is weirdestthing that has been copied
 
-    //     if (executable == NULL) {
-    //       printf("Unable to open file %s\n", filename);
-    //       return;
-    //     }
+        if (filename == NULL) {
+          printf("Unable to open file %s\n", filename);
+          return;
+        }
 
 
-    //     Openfile *executable = filesystem->Openfile(machine->ReadRegister(4));
+        Openfile *executable = fileSystem->Open(filename);
+        AddrSpace *space;
+        if (filename == NULL) {
+          printf("Unable to open file %s\n", filename);
+          return;
+        }
+        space = new AddrSpace(executable);
+        currentThread->space=space;
+        delete executable;
+        space->InitRegisters();
+        space->RestoreState();
         
-    //     AddrSpace *space;
-    //     space = new AddrSpace(executable);
-    //     currentThread->space=space;
-
-    //     space->InitRegisters();
-    //     space->RestoreState();
-    //     delete executable;
-    //     machine->Run();
-    //     ASSERT(FALSE);      
-    //     // machine->Run never returns;
-    //       // the address space exits
-    //       // by doing the syscall "exit"
-    //     //no more incrementing count :)
-    // }
+        machine->Run();
+        ASSERT(FALSE);      
+        // machine->Run never returns;
+          // the address space exits
+          // by doing the syscall "exit"
+        //no more incrementing count :)
+    }
     else {
 	printf("Unexpected user mode exception %d %d\n", which, type);
 	ASSERT(FALSE);
