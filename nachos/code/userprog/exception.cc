2d5b9d4199ab9eac1ca3a9aa5f70f6653d58c4b9
Manikanta Reddy <manikant@cse.iitk.ac.in>
2015-09-25 10:37:47 +0530

    Now able to read from memory. Yet to check if it works

--- nachos/code/userprog/exception.cc
+++ nachos/code/userprog/exception.cc
@@ -308,11 +308,30 @@ ExceptionHandler(ExceptionType which)
     else if((which == SyscallException) && (type == syscall_Exec)){
         //I assume there is something called filename :(
         //where do I find
-        Openfile *executable = filesystem->Openfile(machine->ReadRegister(4));
-        /*if (executable == NULL) {
+
+        //refer to line 130 above to know how to read from memory
+        char filename[100];
+        int i=0;
+
+        vaddr = machine->ReadRegister(4);
+        machine->ReadMem(vaddr, 1, &memval);
+        while ((*(char*)&memval) != '\0') {
+            filename[i]  = (char)memval;
+            ++i;
+            vaddr++;
+            machine->ReadMem(vaddr, 1, &memval);
+        }
+        filename[i]  = (char)memval;
+        //This by far is weirdestthing that has been copied
+
+        if (executable == NULL) {
           printf("Unable to open file %s\n", filename);
           return;
-        }*/
+        }
+
+
+        Openfile *executable = filesystem->Openfile(machine->ReadRegister(4));
+        
         AddrSpace *space;
         space = new AddrSpace(executable);
         currentThread->space=space;
@@ -321,7 +340,8 @@ ExceptionHandler(ExceptionType which)
         space->RestoreState();
         delete executable;
         machine->Run();
-        //ASSERT(FALSE);      // machine->Run never returns;
+        ASSERT(FALSE);      
+        // machine->Run never returns;
           // the address space exits
           // by doing the syscall "exit"
         //no more incrementing count :)
